5463c40df1cb1fa18a140b54713fe0a5593d6373
Sebastian <sebastianbrindmark@gmail.com>
2025-09-21 12:50:42 +0200

    Add Person class to Bankomat namespace
    Introduces a new `Person` class with read-only properties
    `Name` and `Personnummer`, initialized via a constructor.
    Includes necessary using directives for improved organization
    and functionality.

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    public class Person
+    {
+        public string Name { get; }
+        public string Personnummer { get; }
+
+        public Person(string name, string personnummer)
+        {
+            Name = name;
+            Personnummer = personnummer;
+        }
+    }
+
+}
